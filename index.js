a8009364c3f3b69f669fc973c0191dd9caa2ba1c
jahid1604091 <jh409780@gmail.com>
2023-09-19 01:24:19 +0600

    inital

---
+++ index.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+app.get('/',(req,res)=>res.send("Server Up"));
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
