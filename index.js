2ceac7ca4d19e17cab13f5a60e7262acda141eb7
jahid1604091 <jh409780@gmail.com>
2023-09-19 13:55:20 +0600

    users route add

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 app.get('/',(req,res)=>res.send("Server Up"));
+app.get('/users',(req,res)=>res.json([
+    {id:1,name:'John Doe'},
+    {id:2,name:'Jane Doe'}
+]));
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
